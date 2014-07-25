797d9c6f38485b9f540b878312c841baf8336942
Russell Tan <sell_nat@yahoo.com>
2014-07-24 19:35:04 -0700

    Formatted code and added documentation

--- Catbot_ino/Catbot.ino
+++ Catbot_ino/Catbot.ino
@@ -1,66 +1,97 @@
 #include <Servo.h>
-
+//Initialize the servos
 Servo vert; 
 Servo hor;
 
+//Associate the servos with the digital inputs
+//Draw the bounding areas only 3 times
 void setup() { 
   vert.attach(8);
   hor.attach(9); 
+  
   int x = 0;
+  
   do
   {
     migrateImpatient (vert,100,30);
     migrateImpatient (hor ,40 ,30);
     migrateImpatient (vert,150,30);
     migrateImpatient (hor ,100,30);
+    
     x++;
   }while(x < 3);
   
-  migrateImpatient(hor,70,5);
+  migrateImpatient(hor ,70 ,5);
   migrateImpatient(vert,125,5);
 }
 
-void migrate(Servo &myServo, int newPos) {
-  int wait=random(0,30); //randomize the wait to make it more interesting
-  int pos = myServo.read(); //Read the current servo position
-  if (pos < newPos) { 
-    for (int i=pos; i < newPos; i++) {
+void migrate(Servo &myServo, int newPos) 
+{
+  //Random wait time
+  int wait=random(0,30); 
+  
+  //Get current position of servos
+  int pos = myServo.read();
+  
+  if (pos < newPos) 
+  { 
+    for (int i=pos; i < newPos; i++) 
+    {
       myServo.write(i);
       delay(wait); 
     }
-  } else { 
-    for (int i=pos; i > newPos; i--) { 
+  } 
+  else 
+  { 
+    for (int i=pos; i > newPos; i--) 
+    { 
       myServo.write(i);
       delay(wait);
     }
   }
 }
 
-void migrateImpatient(Servo &myServo, int newPos, int waitTime) {
-  int pos = myServo.read(); //Read the current servo position
-  if (pos < newPos) { 
-    for (int i=pos; i < newPos; i++) {
+//Same function as above but code can specify a wait time 
+//Used for the initial bounding sector
+void migrateImpatient(Servo &myServo, int newPos, int waitTime) 
+{
+  int pos = myServo.read();
+  
+  if (pos < newPos) 
+  { 
+    for (int i=pos; i < newPos; i++) 
+    {
       myServo.write(i);
       delay(waitTime); 
     }
-  } else { 
-    for (int i=pos; i > newPos; i--) { 
+  } 
+  else 
+  { 
+    for (int i=pos; i > newPos; i--) 
+    { 
       myServo.write(i);
       delay(waitTime);
     }
   }
 }
     
-  
-void randomPosition() {
-  int rand=random(40,100); //The range is limited to 60 deg for better cat action
+//Generates random coordinates  
+void randomPosition() 
+{
+  //Horizontal servo is limited to 60 degrees
+  int rand=random(40,100); 
   migrate(hor, rand);
   
-  rand=random(100,150); //The vertical range is limited to 45 deg also for better cat action. 
+  //The vertical range is limited to 50 degrees
+  rand=random(100,150); 
   migrate(vert, rand);
 }
    
-void loop() { 
+//Carries out the random movements with varied wait times
+void loop() 
+{ 
+  //Specifies the wait time between each new set of coords
+  //Currently between 100ms-750ms
   int rand = random(100,750);
   
   randomPosition();
